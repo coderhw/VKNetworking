584e474de753ba672101997f04fa24f43e1bb546
huwen126 <huwen126@126.com>
2017-08-24 11:22:42 +0800

    start

---
+++ VKNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKNetworking
+//
+//  Created by vanke on 2017/8/24.
+//  Copyright © 2017年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
